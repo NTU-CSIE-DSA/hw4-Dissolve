b04d0220d30ea538effa9e8cdff3306a840babe0
yungyaoc <yycheng910@gmail.com>
2024-05-14 00:35:53 +0800

    fix gen_worst.cpp (plz this time is correct)

--- gen/gen_worst.cpp
+++ gen/gen_worst.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]){
     bool haveOp[4] = {atoi(argv[4]), atoi(argv[5]), atoi(argv[6]), atoi(argv[7])};
 
     int mergeCnt = 0;
-    vector<set<int>> v[21];
+    vector<vector<int>> v[21];
     for (int i=1;i<=N;++i) v[0].push_back({i});
     cout << N << ' ' << Q << '\n';
     for (int i=1;i<=N;++i) cout << rnd.next(1ll, (ll)1e12) << " \n"[i==N];
@@ -19,51 +19,49 @@ int main(int argc, char *argv[]){
         while (!haveOp[op-1]) op = rnd.next(1, 4);
 
         if (op == 1){
-            int can1 = -1, can1i, can1j, can2 = -1, can2i, can2j;
+            int can1 = -1, can2 = -1;
             for (int i=0;i<20;++i){
-                for (int j=0;j<v[i].size();++j){
-                    if (can1 == -1){
-                        can1 = *(pick ? prev(v[i][j].end()) : v[i][j].begin());
-                        can1i = i;
-                        can1j = j;
-                    }
-                    else{
-                        can2 = *(pick ? prev(v[i][j].end()) : v[i][j].begin());
-                        can2i = i;
-                        can2j = j;
-                        break;
-                    }
+                if (v[i].size() > 1){
+                    auto nv = v[i].back();
+                    can1 = rnd.choice(nv);
+                    v[i].pop_back();
+                    can2 = rnd.choice(v[i].back());
+                    for (auto &x:nv) v[i].back().push_back(x);
+                    v[i+1].push_back(vector<int>(0));
+                    v[i+1].back().swap(v[i].back());
+                    v[i].pop_back();
                 }
-                if (can2 != -1) break;
             }
-            if (can2 == -1) can2 = rnd.choice(v[can1i][can1j]);
-            else if (can1i != can2i || can1j != can2j){
-                set <int> ns;
-                for (auto &t:v[can1i][can1j]) v[can2i][can2j].insert(t);
-                v[can2i+1].push_back({});
-                v[can2i+1].back().swap(v[can2i][can2j]);
-                v[can2i].erase(v[can2i].begin() + can2j);
-                v[can1i].erase(v[can1i].begin() + can1j);
+            if (can1 == -1){
+                can1 = rnd.next(1, N);
+                can2 = rnd.next(1, N);
+                while (can1 == can2) can2 = rnd.next(1, N);
             }
             cout << "1 " << can1 << ' ' << can2;
         }
         else if (op == 2){
             cout << "2 ";
-            for (int i=19;i>=0;--i) if (!v[i].empty()){
+            for (int i=20;i>=0;--i) if (!v[i].empty()){
                 int j = rnd.next(0, (int)v[i].size()-1);
 
-                if (pick) cout << *prev(v[i][j].end()) << ' ' << rnd.next(1, (int)1e6);
-                else cout << *v[i][j].begin() << ' ' << rnd.next(1, (int)1e6);
+                if (rnd.next(0, 1)){
+                    if (pick) cout << *(v[i][j].end()-1) << ' ' << rnd.next(1, (int)1e6);
+                    else cout << *v[i][j].begin() << ' ' << rnd.next(1, (int)1e6);
+                }
+                else cout << rnd.choice(v[i][j]);
                 break;
             }
         }
         else if (op == 3){
             cout << "3 ";
-            for (int i=19;i>=0;--i) if (!v[i].empty()){
+            for (int i=20;i>=0;--i) if (!v[i].empty()){
                 int j = rnd.next(0, (int)v[i].size()-1);
 
-                if (pick) cout << *prev(v[i][j].end());
-                else cout << *v[i][j].begin();
+                if (rnd.next(0, 1)){
+                    if (pick) cout << *(v[i][j].end()-1);
+                    else cout << *v[i][j].begin();
+                }
+                else cout << rnd.choice(v[i][j]);
                 break;
             }
         }
